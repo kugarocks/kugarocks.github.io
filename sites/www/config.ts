811151caf781ec1c4e483d0c19b975038c268d73
kugarocks <kugacola@gmail.com>
2025-05-03 13:04:41 +0800

    init vitepress

---
+++ sites/www/config.ts
@@ -0,0 +1,178 @@
+import { createRequire } from 'module'
+import { defineAdditionalConfig, type DefaultTheme } from 'vitepress'
+
+const require = createRequire(import.meta.url)
+const pkg = require('vitepress/package.json')
+
+export default defineAdditionalConfig({
+  lang: 'en-US',
+  description: 'Vite & Vue powered static site generator.',
+
+  themeConfig: {
+    nav: nav(),
+
+    sidebar: {
+      '/guide/': { base: '/guide/', items: sidebarGuide() },
+      '/reference/': { base: '/reference/', items: sidebarReference() }
+    },
+
+    editLink: {
+      pattern: 'https://github.com/vuejs/vitepress/edit/main/docs/:path',
+      text: 'Edit this page on GitHub'
+    },
+
+    footer: {
+      message: '<a href="https://beian.miit.gov.cn/" target="_blank">粤ICP备2024312555号-1</a>',
+      copyright: 'Copyright © 2024-present kugarocks'
+    }
+  }
+})
+
+function nav(): DefaultTheme.NavItem[] {
+  return [
+    {
+      text: 'Blog',
+      link: '/guide/what-is-vitepress',
+      activeMatch: '/guide/'
+    },
+    {
+      text: 'Projects',
+      link: '/guide/what-is-vitepress',
+      activeMatch: '/guide/'
+    },
+    {
+      text: 'Cloud Native',
+      items: [
+        {
+          text: 'Kubernetes',
+          link: 'https://work.kugarocks.com'
+        },
+        {
+          text: 'Docker',
+          link: 'https://work.kugarocks.com'
+        },
+      ]
+    },
+    {
+      text: 'Middlewares',
+      items: [
+        {
+          text: 'Nginx',
+          link: 'https://work.kugarocks.com'
+        },
+        {
+          text: 'Redis',
+          link: 'https://work.kugarocks.com'
+        },
+        {
+          text: 'MySQL',
+          link: 'https://work.kugarocks.com'
+        },
+      ]
+    },
+    {
+      text: 'Books',
+      items: [
+        {
+          text: 'Linux Basic',
+          link: 'https://work.kugarocks.com'
+        },
+      ]
+    },
+    {
+      text: 'Links',
+      items: [
+        {
+          text: 'kugaworks',
+          link: 'https://work.kugarocks.com'
+        },
+        {
+          text: 'kugamusic',
+          link: 'https://music.kugarocks.com'
+        }
+      ]
+    }
+  ]
+}
+
+function sidebarGuide(): DefaultTheme.SidebarItem[] {
+  return [
+    {
+      text: 'Introduction',
+      collapsed: false,
+      items: [
+        { text: 'What is VitePress?', link: 'what-is-vitepress' },
+        { text: 'Getting Started', link: 'getting-started' },
+        { text: 'Routing', link: 'routing' },
+        { text: 'Deploy', link: 'deploy' }
+      ]
+    },
+    {
+      text: 'Writing',
+      collapsed: false,
+      items: [
+        { text: 'Markdown Extensions', link: 'markdown' },
+        { text: 'Asset Handling', link: 'asset-handling' },
+        { text: 'Frontmatter', link: 'frontmatter' },
+        { text: 'Using Vue in Markdown', link: 'using-vue' },
+        { text: 'Internationalization', link: 'i18n' }
+      ]
+    },
+    {
+      text: 'Customization',
+      collapsed: false,
+      items: [
+        { text: 'Using a Custom Theme', link: 'custom-theme' },
+        {
+          text: 'Extending the Default Theme',
+          link: 'extending-default-theme'
+        },
+        { text: 'Build-Time Data Loading', link: 'data-loading' },
+        { text: 'SSR Compatibility', link: 'ssr-compat' },
+        { text: 'Connecting to a CMS', link: 'cms' }
+      ]
+    },
+    {
+      text: 'Experimental',
+      collapsed: false,
+      items: [
+        { text: 'MPA Mode', link: 'mpa-mode' },
+        { text: 'Sitemap Generation', link: 'sitemap-generation' }
+      ]
+    },
+    { text: 'Config & API Reference', base: '/reference/', link: 'site-config' }
+  ]
+}
+
+function sidebarReference(): DefaultTheme.SidebarItem[] {
+  return [
+    {
+      text: 'Reference',
+      items: [
+        { text: 'Site Config', link: 'site-config' },
+        { text: 'Frontmatter Config', link: 'frontmatter-config' },
+        { text: 'Runtime API', link: 'runtime-api' },
+        { text: 'CLI', link: 'cli' },
+        {
+          text: 'Default Theme',
+          base: '/reference/default-theme-',
+          items: [
+            { text: 'Overview', link: 'config' },
+            { text: 'Nav', link: 'nav' },
+            { text: 'Sidebar', link: 'sidebar' },
+            { text: 'Home Page', link: 'home-page' },
+            { text: 'Footer', link: 'footer' },
+            { text: 'Layout', link: 'layout' },
+            { text: 'Badge', link: 'badge' },
+            { text: 'Team Page', link: 'team-page' },
+            { text: 'Prev / Next Links', link: 'prev-next-links' },
+            { text: 'Edit Link', link: 'edit-link' },
+            { text: 'Last Updated Timestamp', link: 'last-updated' },
+            { text: 'Search', link: 'search' },
+            { text: 'Carbon Ads', link: 'carbon-ads' }
+          ]
+        }
+      ]
+    }
+  ]
+}
---
+++ sites/www/en/index.md
@@ -0,0 +1,28 @@
+---
+layout: home
+
+hero:
+  name: kugarocks
+  text: 
+  tagline: I love coding and music. Here is where I log my life. Making little stuff and playing guitar.
+  actions:
+    - theme: brand
+      text: Rednote
+      link: https://www.xiaohongshu.com/user/profile/65683d68000000001c01b1e5
+    - theme: alt
+      text: GitHub
+      link: https://github.com/vuejs/vitepress
+  image:
+    src: /guitar-gopher.png
+    alt: guitar-gopher
+
+features:
+  - title: Focus on Your Content
+    details: Effortlessly create beautiful documentation sites with just markdown.
+  - title: Enjoy the Vite DX
+    details: Instant server start, lightning fast hot updates, and leverage Vite ecosystem plugins.
+  - title: Customize with Vue
+    details: Use Vue syntax and components directly in markdown, or build custom themes with Vue.
+  - title: Ship Fast Sites
+    details: Fast initial load with static HTML, fast post-load navigation with client-side routing.
+---
